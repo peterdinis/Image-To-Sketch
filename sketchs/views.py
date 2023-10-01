ee67ceaa59e0a891489e4c10b10501cd4935e684
Peterdinis <peterdinis0@pobox.sk>
2023-10-01 14:45:07 +0200

    feat: First migrations for database

--- sketchs/views.py
+++ sketchs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def searchForSketch(request):
+    pass
+
+def listAllSketchs(request):
+    pass
+
+def sketchDetail(request):
+    pass
+
+
+def generateNewSketch(request):
+    pass
