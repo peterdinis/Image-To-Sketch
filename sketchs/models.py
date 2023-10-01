8124bbd66b074858d34b93b40b648bb61464b211
Peterdinis <peterdinis0@pobox.sk>
2023-10-01 17:54:04 +0200

    fix: Fix small problem with migration

--- sketchs/models.py
+++ sketchs/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+
+def user_directory_path(instance, filename):
+  
+    # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
 # Create your models here.
 class Sketch(models.Model):
     name = models.CharField(max_length = 200)
+    upload = models.ImageField(default="", upload_to = user_directory_path)
