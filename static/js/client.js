8ac84a1ea25c6feab94688a381ca40a64a1aeb3c
Peterdinis <peterdinis0@pobox.sk>
2023-10-07 18:53:44 +0200

    feat: Design profile page wip

--- static/js/client.js
+++ static/js/client.js
@@ -1,26 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // open
+document.addEventListener('DOMContentLoaded', function () {
+    // Open
     const burger = document.querySelectorAll('.navbar-burger');
     const menu = document.querySelectorAll('.navbar-menu');
 
     if (burger.length && menu.length) {
-        for (var i = 0; i < burger.length; i++) {
-            burger[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
+        for (let i = 0; i < burger.length; i++) {
+            burger[i].addEventListener('click', function () {
+                for (let j = 0; j < menu.length; j++) {
                     menu[j].classList.toggle('hidden');
                 }
             });
         }
     }
 
-    // close
+    // Close
     const close = document.querySelectorAll('.navbar-close');
     const backdrop = document.querySelectorAll('.navbar-backdrop');
 
     if (close.length) {
-        for (var i = 0; i < close.length; i++) {
-            close[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
+        for (let i = 0; i < close.length; i++) {
+            close[i].addEventListener('click', function () {
+                for (let j = 0; j < menu.length; j++) {
                     menu[j].classList.toggle('hidden');
                 }
             });
@@ -28,15 +28,12 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     if (backdrop.length) {
-        for (var i = 0; i < backdrop.length; i++) {
-            backdrop[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
+        for (let i = 0; i < backdrop.length; i++) {
+            backdrop[i].addEventListener('click', function () {
+                for (let j = 0; j < menu.length; j++) {
                     menu[j].classList.toggle('hidden');
                 }
             });
         }
     }
 });
-
-/* Sidebar for sketchs */
-
