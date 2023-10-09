30787789694399da0f378ec937c2ec71576f7186
Peterdinis <peterdinis0@pobox.sk>
2023-10-09 19:25:31 +0200

    feat: Search endpoint done

--- sketchs/urls.py
+++ sketchs/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("generate/", views.generateNewSketch, name="sketch_generate"),
     path('list/', views.sketch_list, name='sketch_list'),
-    path('sketches/search/', views.sketch_search, name='sketch_search'),
+    path('search/', views.sketch_search, name='sketch_search'),
 ]
