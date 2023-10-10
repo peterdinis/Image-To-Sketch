2cf46ef6ba4d99ad55c5ed2e0d0bb4a3ee21750d
Peterdinis <peterdinis0@pobox.sk>
2023-10-10 18:25:58 +0200

    feat: Writing new logic for convert to sketch image wip

--- sketchs/views.py
+++ sketchs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Sketch
+import cv2
 
 def sketch_list(request):
     sketches = Sketch.objects.all()
@@ -19,4 +20,41 @@ def sketch_search(request):
 
 
 def generate_sketch(request):
+    # Read the original image
+    image1 = cv2.imread('image.jpg')
+    
+    # Display the original image
+    window_name = 'Actual image'
+    cv2.imshow(window_name, image1)
+
+    # Convert the image to grayscale
+    grey_img = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+    
+    # Invert the grayscale image
+    invert = cv2.bitwise_not(grey_img)
+    
+    # Apply Gaussian blur to the inverted image
+    blur = cv2.GaussianBlur(invert, (21, 21), 0)
+    
+    # Invert the blurred image
+    invertedblur = cv2.bitwise_not(blur)
+    
+    # Create the sketch by dividing the grayscale image by the inverted blurred image
+    sketch = cv2.divide(grey_img, invertedblur, scale=256.0)
+    
+    # Save the sketch as "sketch.jpeg"
+    cv2.imwrite("sketch.jpeg", sketch)
+    
+    # Read the saved sketch image
+    image = cv2.imread("sketch.jpeg")
+    
+    # Display the sketch image
+    window_name = 'Sketch image'
+    cv2.imshow(window_name, image)
+    
+    # Wait for a key press and then close the OpenCV windows
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    # Render the Django template
     return render(request, 'sketchs/generate.html')
