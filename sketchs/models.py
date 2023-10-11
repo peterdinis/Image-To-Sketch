85783b4c88d5445b52baebcd1ce75f58b0fe7b9d
Peterdinis <peterdinis0@pobox.sk>
2023-10-11 19:40:54 +0200

    FOO

--- sketchs/models.py
+++ sketchs/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 class Sketch(models.Model):
     name = models.CharField(max_length=200)
-    test = models.CharField(max_length=200)
     image = models.ImageField(default='')
 
     def __str__(self):
