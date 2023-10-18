fdf17de3e984a9be1a1fc4383184ed8c1eaa039a
Peterdinis <peterdinis0@pobox.sk>
2023-10-18 18:37:27 +0200

    FOO

--- accounts/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-## Testing login / register endpoints
-
-class AccountTestCase(TestCase):
-    pass
--- sketchs/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-## Example tests and sketchs endpoints
-
-class SketchsTestCase(TestCase):
-    pass
